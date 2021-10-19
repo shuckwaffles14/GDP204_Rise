d452840cb7d4a6528b3d90edb40a721dee8aa97d
shuckwaffles14 <72298574+shuckwaffles14@users.noreply.github.com>
2021-10-20 10:20:19 +1100

    Basic Menu
    & Splash Screen Test

---
+++ Rise_Assessment/Assets/Script/splashScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class splashScreen : MonoBehaviour
+{
+    public Image splashImage;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+    void FadeOut()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
