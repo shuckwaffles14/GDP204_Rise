bbe47164b8b42dc547601c448f282c3a33d672fd
CatchyCar <72535542+CatchyCar@users.noreply.github.com>
2021-10-31 21:00:29 +1100

    Parallax In

--- Rise_Assessment/Assets/Script/Parallax.cs
+++ Rise_Assessment/Assets/Script/Parallax.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paralex : MonoBehaviour
+public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    void Update() {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
