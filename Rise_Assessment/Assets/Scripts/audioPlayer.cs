1cee626e766b9051b08ce2d38ecbbf99dd1206f9
CatchyCar <72535542+CatchyCar@users.noreply.github.com>
2021-11-07 03:55:36 +1100

    Add some stuff
    background, animations, added sound files (check player sound and environmental sound)

---
+++ Rise_Assessment/Assets/Scripts/audioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioPlayer : MonoBehaviour
+{
+    public AudioClip impact;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter()
+    {
+        audioSource.PlayOneShot(impact, 3F);
+    }
+}
