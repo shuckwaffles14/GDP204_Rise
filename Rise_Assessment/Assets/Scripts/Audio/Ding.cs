3bee79a944e74bf8307ecd3a148b62c553ded750
shuckwaffles14 <72298574+shuckwaffles14@users.noreply.github.com>
2021-12-05 15:44:33 +1100

    fixes :3

---
+++ Rise_Assessment/Assets/Scripts/Audio/Ding.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ding : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<AudioSource>().Play();
+
+    }
+}
