666bcf082548accf9ff6e2bfda64f796597b67e3
AngeloE28 <66254064+AngeloE28@users.noreply.github.com>
2022-09-01 23:40:11 +1000

    Player Shadow

---
+++ Assets/Scripts/Player/ShadowFlip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowFlip : MonoBehaviour
+{
+    [SerializeField] private Transform _shadowCaster;
+    private SpriteRenderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float xScale = _renderer.flipX ? -1 : 1;
+        Vector3 newScale = new Vector3(xScale, _shadowCaster.localScale.y, _shadowCaster.localScale.z);
+        _shadowCaster.localScale = newScale;
+    }
+}
